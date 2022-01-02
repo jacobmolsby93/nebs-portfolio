bdaf266185a1c7a70ef483aafc94e4c0422ffe39
Jacob Molsby <jacobmolsby@hotmail.se>
2022-01-02 14:53:48 +0000

    Deployment part 5

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -96,11 +96,6 @@ WSGI_APPLICATION = 'portfolio_site.wsgi.application'
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
-AUTHENTICATION_BACKENDS = [
-    # Needed to login by username in Django admin, regardless of `allauth`
-    'django.contrib.auth.backends.ModelBackend',
-
-]
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
