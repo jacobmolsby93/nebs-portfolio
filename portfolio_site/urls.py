b7e55814510ee9eca255c975446713b76b1e4651
Jacob Molsby <jacobmolsby@hotmail.se>
2022-01-04 11:48:39 +0000

    Fixed about img stretch on certain phone type

--- portfolio_site/urls.py
+++ portfolio_site/urls.py
@@ -21,6 +21,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin-335533/', admin.site.urls),
     path('', include('home.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
